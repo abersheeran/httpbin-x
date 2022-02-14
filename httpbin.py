1c91d915033a10f34b23ce6da4b264f827911f9b
Aber <me@abersheeran.com>
2022-02-14 15:43:42 +0800

    Update httpbin.py

--- httpbin.py
+++ httpbin.py
@@ -1,41 +1,55 @@
 import traceback
+from typing import Awaitable, Callable
 
-from baize.asgi import request_response, Request, JSONResponse, PlainTextResponse
+from baize.asgi import request_response, middleware, Request, Response, JSONResponse, PlainTextResponse
+from baize.datastructures import UploadFile
 from baize.exceptions import HTTPException
+
+
+@middleware
+async def catch_exception(request: Request, next_call: Callable[[Request], Awaitable[Response]]) -> Response:
+    try:
+        response = await next_call(request)
+    except BaseException:
+        return PlainTextResponse(traceback.format_exc(), 500)
 
 
 @request_response
 async def anything(request: Request) -> JSONResponse:
     try:
-        try:
-            form = list((await request.form).multi_items())
-        except HTTPException:
-            form = {}
+        form = [
+            (
+                name,
+                value if isinstance(value, str) else
+                {"filename": value.filename, "content-type": value.content_type}
+            )
+            for name, value in (await request.form).multi_items()
+        ]
+    except HTTPException:
+        form = {}
 
-        try:
-            json = await request.json
-        except HTTPException:
-            json = {}
+    try:
+        json = await request.json
+    except HTTPException:
+        json = {}
 
-        try:
-            if not (form or json):
-                content = (await request.body).decode()
-            else:
-                content = ""
-        except UnicodeDecodeError:
+    try:
+        if not (form or json):
+            content = (await request.body).decode()
+        else:
             content = ""
+    except UnicodeDecodeError:
+        content = ""
 
-        return JSONResponse(
-            {
-                "method": request.method,
-                "urlpath": request.url.path,
-                "query_params": list(request.query_params.multi_items()),
-                "headers": dict(request.headers),
-                "cookies": request.cookies,
-                "form": form,
-                "json": json,
-                "content": content,
-            }
-        )
-    except BaseException:
-        return PlainTextResponse(traceback.format_exc(), 500)
+    return JSONResponse(
+        {
+            "method": request.method,
+            "urlpath": request.url.path,
+            "query_params": list(request.query_params.multi_items()),
+            "headers": dict(request.headers),
+            "cookies": request.cookies,
+            "form": form,
+            "json": json,
+            "content": content,
+        }
+    )
