55fc7a4c7c63119b4a32d38bf5b9daea73ebd792
Aber <me@abersheeran.com>
2022-02-14 14:54:01 +0800

    Update httpbin.py

--- httpbin.py
+++ httpbin.py
@@ -7,36 +7,35 @@ from baize.exceptions import HTTPException
 @request_response
 async def anything(request: Request) -> JSONResponse:
     try:
-        form = list((await request.form).multi_items())
-    except HTTPException:
-        form = {}
-    except BaseException:
-        return PlainTextResponse(traceback.format_exc(), 500)
+        try:
+            form = list((await request.form).multi_items())
+        except HTTPException:
+            form = {}
 
-    try:
-        json = await request.json
-    except HTTPException:
-        json = {}
-    except BaseException:
-        return PlainTextResponse(traceback.format_exc(), 500)
+        try:
+            json = await request.json
+        except HTTPException:
+            json = {}
 
-    try:
-        if not (form or json):
-            content = (await request.body).decode()
-        else:
+        try:
+            if not (form or json):
+                content = (await request.body).decode()
+            else:
+                content = ""
+        except UnicodeDecodeError:
             content = ""
-    except UnicodeDecodeError:
-        content = ""
 
-    return JSONResponse(
-        {
-            "method": request.method,
-            "urlpath": request.url.path,
-            "query_params": list(request.query_params.multi_items()),
-            "headers": dict(request.headers),
-            "cookies": request.cookies,
-            "form": form,
-            "json": json,
-            "content": content,
-        }
-    )
+        return JSONResponse(
+            {
+                "method": request.method,
+                "urlpath": request.url.path,
+                "query_params": list(request.query_params.multi_items()),
+                "headers": dict(request.headers),
+                "cookies": request.cookies,
+                "form": form,
+                "json": json,
+                "content": content,
+            }
+        )
+    except BaseException:
+        return PlainTextResponse(traceback.format_exc(), 500)
