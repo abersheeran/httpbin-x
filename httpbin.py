950f94e1609021c6336af27e86ca258b07e7142b
Aber <me@abersheeran.com>
2022-06-24 10:01:39 +0800

    black it

--- httpbin.py
+++ httpbin.py
@@ -1,13 +1,22 @@
 import traceback
 from typing import Awaitable, Callable
 
-from baize.asgi import request_response, middleware, Request, Response, JSONResponse, PlainTextResponse
+from baize.asgi import (
+    request_response,
+    middleware,
+    Request,
+    Response,
+    JSONResponse,
+    PlainTextResponse,
+)
 from baize.datastructures import UploadFile
 from baize.exceptions import HTTPException
 
 
 @middleware
-async def catch_exception(request: Request, next_call: Callable[[Request], Awaitable[Response]]) -> Response:
+async def catch_exception(
+    request: Request, next_call: Callable[[Request], Awaitable[Response]]
+) -> Response:
     try:
         return await next_call(request)
     except BaseException:
@@ -20,9 +29,9 @@ async def anything(request: Request) -> JSONResponse:
     try:
         form = [
             (
-                name,
-                value if isinstance(value, str) else
                 {"filename": value.filename, "content-type": value.content_type}
+                if isinstance(value, UploadFile)
+                else (name, value),
             )
             for name, value in (await request.form).multi_items()
         ]
@@ -53,5 +62,6 @@ async def anything(request: Request) -> JSONResponse:
             "form": form,
             "json": json,
             "content": content,
-        }
+        },
+        indent=2,
     )
